86bc3667866a540383c99cd12533cbcd27fe1829
Alejandro González <alejandro.gon.per@gmail.com>
2017-01-31 01:22:31 +0100

    Moved all update tasks to services. Generated notifications.

--- api/controllers/AtcController.js
+++ api/controllers/AtcController.js
@@ -6,10 +6,36 @@
  */
 
 
+const nodemailer = require('@nodemailer/pro');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'alejandro@hubcivico.org',
+    pass: 'igyxycvntuvbztts'
+  }
+});
 
+var ok = {
+  from: '"OpenVademecum?" <hello@openvademecum.es>', // sender address
+  to: 'alejandro.gon.per@gmail.com', // list of receivers
+  subject: 'Cron finished', // Subject line
+  text: 'The update was correct so far.' // plain text body
+};
+
+var ko = {
+  from: '"OpenVademecum?" <hello@openvademecum.es>', // sender address
+  to: 'alejandro.gon.per@gmail.com', // list of receivers
+  subject: 'Cron errored', // Subject line
+  text: 'The update failed.' // plain text body
+};
 module.exports = {
   update: function(req, res){
-    atcUtil.update().then(function(){sails.log.info('[CRON] - Finished updating ATC.')}).catch(function(err){sails.log.error('[ERROR] - '+err)})
+    transporter.sendMail(ok, function(error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      sails.log.info('Message %s sent: %s', info.messageId, info.response);
+    });
   }
 };
 
--- config/cron.js
+++ config/cron.js
@@ -1,147 +1,82 @@
+const nodemailer = require('@nodemailer/pro');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  }
+});
 
+var ok = {
+  from: '"OpenVademecum?" <hello@openvademecum.es>', // sender address
+  to: 'alejandro.gon.per@gmail.com', // list of receivers
+  subject: 'Cron finished', // Subject line
+  text: 'The update was correct so far.' // plain text body
+};
 
+var ko = {
+  from: '"OpenVademecum?" <hello@openvademecum.es>', // sender address
+  to: 'alejandro.gon.per@gmail.com', // list of receivers
+  subject: 'Cron errored', // Subject line
+  text: 'The update failed.' // plain text body
+};
 module.exports.cron = {
   pull: {
-    schedule: '* * * * *',
+    schedule: '0 * * * *',
     onTick: function () {
-      pullUtil.pull().then(function(){sails.log.info('[CRON] - Finished updating ATC.')}).catch(function(err){sails.log.error('[ERROR] - '+err)});
-
+      pullUtil.pull().then(function () {
+        atcUtil.update().then(function () {
+          dcpUtil.update().then(function () {
+            dcpfUtil.update().then(function () {
+              dcsaUtil.update().then(function () {
+                envasesUtil.update().then(function () {
+                  excipientesUtil.update().then(function () {
+                    ffarmaceuticasimpUtil.update().then(function () {
+                      ffarmaceuticaUtil.update().then(function () {
+                        laboratorioUtil.update().then(function () {
+                          pactivosUtil.update().then(function () {
+                            sitregistroUtil.update().then(function () {
+                              unicontUtil.update().then(function () {
+                                vadmonUtil.update().then(function () {
+                                  prescripcionUtil.update().then(function () {
+                                    sendOkMail();
+                                  }).catch(function (err) {sendErrorMail(err)});
+                                }).catch(function (err) {sendErrorMail(err)});
+                              }).catch(function (err) {sendErrorMail(err)});
+                            }).catch(function (err) {sendErrorMail(err)});
+                          }).catch(function (err) {sendErrorMail(err)});
+                        }).catch(function (err) {sendErrorMail(err)});
+                      }).catch(function (err) {sendErrorMail(err)});
+                    }).catch(function (err) {sendErrorMail(err)});
+                  }).catch(function (err) {sendErrorMail(err)});
+                }).catch(function (err) {sendErrorMail(err)});
+              }).catch(function (err) {sendErrorMail(err)});
+            }).catch(function (err) {sendErrorMail(err)});
+          }).catch(function (err) {sendErrorMail(err)});
+        }).catch(function (err) {sendErrorMail(err)});
+      }).catch(function (err) {sendErrorMail(err)});
     },
-    start: false,
+    start: true,
     timezone: 'Europe/Madrid',
     context: undefined
-  },
-
-  updateATC: {
-    schedule: '* * * * *',
-    onTick: function () {
-      atcUtil.update().then(function(){sails.log.info('[CRON] - Pull done.')}).catch(function(err){sails.log.error('[ERROR] - '+err)});
-    },
-    start: false, // Start task immediately
-    timezone: 'Europe/Madrid', // Custom timezone
-    context: undefined // Custom context for onTick callback
-
-  },
-  /*updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   },
-   updateATC: {
-   schedule: '* * * * *',
-   onTick: function () {
-   sails.log.info('[CRON] - Pulling new data from AEMPS');
-   },
-   start: false, // Start task immediately
-   timezone: 'Europe/Madrid', // Custom timezone
-   context: undefined // Custom context for onTick callback
-
-   }*/
-
-
+  }
 };
+
+function sendErrorMail(err) {
+  sails.log.error('[ERROR] - ' + err);
+  transporter.sendMail(ko, function (error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    sails.log.info('Message %s sent: %s', info.messageId, info.response);
+  });
+}
+
+function sendOkMail(){
+  transporter.sendMail(ok, function (error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    sails.log.info('Message %s sent: %s', info.messageId, info.response);
+  });
+}
