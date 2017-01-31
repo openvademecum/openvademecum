3df7e8cde1d5f81e51a1bd7740c12a2ed5a3af8f
Alejandro González <alejandro.gon.per@gmail.com>
2017-01-31 17:02:57 +0100

    Testing in heroku

--- api/services/pullUtil.js
+++ api/services/pullUtil.js
@@ -11,12 +11,8 @@ const fs = require('fs');
 const request = require('request');
 const unzip = require('unzip');
 const fstream = require('fstream');
-const xml2js = require('xml2js');
-const path = require("path");
-const jsonfile = require('jsonfile');
 
 var now = Date.now();
-
 
 module.exports.pull = function () {
   return new Promise(function (resolve, reject) {
@@ -30,43 +26,12 @@ module.exports.pull = function () {
         readStream.pipe(unzip.Parse()).pipe(writeStream).on('close', function () {
           sails.log.info('[CRON] - Unzipped to folder, timestamp: ' + now);
           fs.unlink(now + '.zip', function (err) {
-            if (err) {
+            if (err){
               sails.log.error("[CRON] - Error while unziping: " + err);
               reject(err);
             }
             sails.log.info('[CRON] - Successfully cleaned.');
-            sails.log.info('[CRON] - Converting to JSON');
-            var p = 'data/';
-            fs.readdir(p, function (err, files) {
-              if (err) {
-                sails.log.error("[CRON] - Error " + err);
-                reject(err);
-              }
-              files.map(function (file) {
-                return path.join(p, file);
-              }).filter(function (file) {
-                return fs.statSync(file).isFile() && path.extname(file) === '.xml';
-              }).forEach(function (file) {
-                var parser = new xml2js.Parser({explicitArray: false});
-                console.log("%s (%s)", file, path.extname(file));
-                var data = fs.readFileSync(file);
-                parser.parseString(data, function (err, data) {
-                  var fileNameSplit = file.split('.');
-                  var fileNameName = fileNameSplit[0];
-                  var fileName = fileNameName + '.json';
-                  jsonfile.writeFileSync(fileName, data);
-                  fs.unlink(file, function (err) {
-                    if (err) {
-                      sails.log.error("[CRON] - Error while unziping: " + err);
-                      reject(err);
-                    }
-                  })
-                })
-
-              });
-              sails.log.info("[CRON] - JOB FINISHED!!!!!!");
-              resolve();
-            });
+            resolve();
           })
         })
       })
