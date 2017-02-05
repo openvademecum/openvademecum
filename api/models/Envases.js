4382cf18c4c748e7cdcfd8fb2ae33747ceac6c5b
Alejandro González <alejandro.gon.per@gmail.com>
2017-02-05 02:13:47 +0100

    DCPF

--- api/models/Envases.js
+++ api/models/Envases.js
@@ -8,6 +8,11 @@
 module.exports = {
 
   attributes: {
+    id: {
+      type: 'integer',
+      unique: true,
+      primaryKey: true
+    },
     cod_envase: {
       type:'integer',
       unique: true
@@ -15,25 +20,6 @@ module.exports = {
     envase: {
       type:'string'
     }
-  },
-  updateOrCreate: function(criteria, values, cb){
-    var self = this; // reference for use by callbacks
-    // If no values were specified, use criteria
-    if (!values) values = criteria.where ? criteria.where : criteria;
-    self.findOne(criteria, function (err, result){
-      if(err) return cb(err);
-      if(result){
-        self.update(criteria, values).exec(function (err, res){
-          if (err) return cb(err);
-          cb(null, res);
-        });
-      }else{
-        self.create(values).exec(function (err, res){
-          if (err) return cb(err);
-          cb(null, res);
-        });
-      }
-    });
   }
 };
 
--- api/services/dcpfUtil.js
+++ api/services/dcpfUtil.js
@@ -8,68 +8,127 @@
  */
 
 const fs = require('fs');
+const _ = require('lodash');
 const XmlStream = require('xml-stream');
 
-module.exports.update = function () {
 
+/******************Model Variables**********************/
+
+const itemName = 'dcpf';
+const modelName = 'dcpf';
+const itemIdName = 'codigodcpf';
+const xmlFile = 'data/DICCIONARIO_DCPF.xml';
+const endCollection = 'aemps_prescripcion_dcpf';
+
+/************************END*****************************/
+
+
+module.exports.update = function () {
   return new Promise(function (resolve, reject) {
-    sails.log.info('[CRON] - Destroying DCPF Collection.');
-    Dcpf.destroy().exec(function (err) {
-      if (err) {
-        sails.log.error("[CRON] - Error while destroying DCPF.");
-        reject();
-      }
-      else {
-        sails.log.info('[CRON] - Updating DCPF.');
-        var stream = fs.createReadStream('data/DICCIONARIO_DCPF.xml');
+
+    var insertedIds = [];
+    var updatedIds = [];
+    var allIds = [];
+    var ids = [];
+
+    sails.log.info("[CRON] - [DCPF] - Updating DCPF...");
+
+
+    //Get all Collection IDs
+    Dcpf.native(function (err, collection) {
+      if (err) reject(err);
+      collection.aggregate([{$group: {_id: null, ids: {$addToSet: "$_id"}}}]).toArray(function (err, results) {
+        if (err) reject(err);
+        else if (results[0] && results[0].hasOwnProperty('ids')) ids = results[0].ids;
+
+        var stream = fs.createReadStream(xmlFile);
         var xml = new XmlStream(stream);
-        xml.collect('dcpf');
-        xml.on('endElement: dcpf', function (item) {
+        //Create or update of all entries on xml
+        xml.collect(itemName);
+        xml.on('endElement: ' + itemName, function (item) {
           xml.pause();
-          Dcpf.create(item).exec(function (err, data) {
+
+          var element = item;
+          var id = item[itemIdName];
+          delete item[itemIdName];
+          item._id = id;
+          allIds.push(item._id);
+
+          Dcpf.findOne({_id: item._id}).exec(function (err, oldItem) {
             if (err) reject(err);
-            else {
-              xml.resume();
-            }
-          })
+
+            Dcpf.native(function (err, collection) {
+              if (err) reject(err);
+
+              collection.updateOne({codigodcpf: id}, {$set: item}, {upsert: true}, function (err, results) {
+                if (err) reject(err);
+
+                results = JSON.parse(results);
+                if (results.hasOwnProperty('upserted')) {
+                  //Item inserted
+                  insertedIds.push(results.upserted[0]._id);
+                  Updates.create({
+                    model: modelName,
+                    new_item: element,
+                    inserted: true,
+                    updated: false,
+                    deleted: false
+                  }).exec(function (err, results) {
+                    if (err) reject(err);
+                    xml.resume()
+                  })
+                } else if (results['nModified'] == 1) {
+                  //Item modified
+                  updatedIds.push(item._id);
+                  Updates.create({
+                    model: modelName,
+                    old_item: oldItem,
+                    new_item: element,
+                    inserted: false,
+                    updated: true,
+                    deleted: false
+                  }).exec(function (err, results) {
+                    if (err) reject(err);
+                    xml.resume();
+                  })
+                } else xml.resume();
+              })
+            });
+
+          });
+
+
         });
-        xml.on('end', function () {
-          sails.log.info("[CRON] - Finished updating DCPF.");
+        xml.on('endElement: ' + endCollection, function () {
+          //Compare new IDS with old ones.
+          var deletedIds = _.difference(ids, allIds);
+
+          //Delete ids not included.
+          Dcpf.native(function (err, collection) {
+            if (err) reject(err);
+
+            deletedIds.forEach(function (entry) {
+              Dcpf.findOne({_id: entry}).exec(function (err, beforeDelete) {
+                if (err) reject(err);
+
+                collection.deleteOne({_id: entry}, function (err, results) {
+                  if (err) reject(err);
+                  Updates.create({
+                    model: modelName,
+                    old_item: beforeDelete,
+                    inserted: false,
+                    updated: false,
+                    deleted: true
+                  }).exec(function (err, results) {
+                    if (err) reject(err);
+                  })
+                })
+              })
+            })
+          });
           resolve();
         });
-
-
-
-
-
-        // fs.readFile('data/DICCIONARIO_DCPF.xml', function (err, data) {
-        //   parser.parseString(data, function (err, data) {
-        //     var index = data.aemps_prescripcion_dcpf.dcpf;
-        //     var count = 0;
-        //     for (var item in index) {
-        //       if (index.hasOwnProperty(item)) {
-        //         var codigodcpf = index[item].codigodcpf.toString();
-        //         var nombredcpf = index[item].nombredcpf.toString();
-        //         var nombrecortodcpf = index[item].nombrecortodcpf.toString();
-        //         var codigodcp = index[item].codigodcp.toString();
-        //         Dcpf.create({
-        //           codigodcpf: codigodcpf,
-        //           nombredcpf: nombredcpf,
-        //           nombrecortodcpf: nombrecortodcpf,
-        //           codigodcp: codigodcp
-        //         }).exec(function (err, data) {
-        //           if (err) reject(err);
-        //           else {
-        //             count++;
-        //             if (count == index.length) resolve();
-        //           }
-        //         })
-        //       }
-        //     }
-        //   })
-        // })
-
-      }
-    })
+      })
+    });
   });
 };
